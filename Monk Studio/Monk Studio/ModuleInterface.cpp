0bc894871679da8e84f0d8f59fcbb12e127b2ace
himar33 <himbrgo@iesvalldemossa.net>
2021-09-27 22:31:19 +0200

    menu modified

--- Monk Studio/Monk Studio/ModuleInterface.cpp
+++ Monk Studio/Monk Studio/ModuleInterface.cpp
@@ -51,69 +51,79 @@ update_status ModuleInterface::PostUpdate(float dt)
 
 	if (show_demo_window)
 		ImGui::ShowDemoWindow(&show_demo_window);
+
+	static float f = 0.0f;
+	static int counter = 0;
+
+	if (ImGui::BeginMainMenuBar())
 	{
-		static float f = 0.0f;
-		static int counter = 0;
-
-		ImGui::Begin("Configuration", NULL, ImGuiWindowFlags_MenuBar);                          // Create a window called "Hello, world!" and append into it.
-		if (ImGui::BeginMainMenuBar())
+		if (ImGui::BeginMenu("File"))
 		{
-			if (ImGui::BeginMenu("File"))
-			{
-				if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Close", "Ctrl+W")) { /* Do stuff */ }
-				ImGui::EndMenu();
-			}
-			if (ImGui::BeginMenu("View"))
-			{
-				if (ImGui::MenuItem("Something1", "Ctrl+O")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Something2", "Ctrl+S")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Something3", "Ctrl+W")) { /* Do stuff */ }
-				ImGui::EndMenu();
-			}
-			if (ImGui::BeginMenu("Help"))
-			{
-				if (ImGui::MenuItem("Gui Demo")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Documentation")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Download Latest")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Report a Bug")) { /* Do stuff */ }
-				if (ImGui::MenuItem("About")) { /* Do stuff */ }
-				ImGui::EndMenu();
-			}
-
-			ImGui::EndMainMenuBar();
+			if (ImGui::MenuItem("Quit", "ESC")) return UPDATE_STOP;
+			ImGui::EndMenu();
 		}
-		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-		ImGui::Checkbox("Another Window", &show_another_window);
-
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-			counter++;
-		ImGui::SameLine();
-		ImGui::Text("counter = %d", counter);
-
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::End();
-
-
-		if (show_another_window)
+		if (ImGui::BeginMenu("View"))
 		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
+			if (ImGui::MenuItem("Console", "1", show_console))
+			{
+				show_console = !show_console;
+			}
+			if (ImGui::MenuItem("Configuration", "4")) { /* Do stuff */ }
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Help"))
+		{
+			if (ImGui::MenuItem("Gui Demo")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Documentation")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Download Latest")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Report a Bug")) { /* Do stuff */ }
+			if (ImGui::MenuItem("About")) { /* Do stuff */ }
+			ImGui::EndMenu();
 		}
 
-		ImGui::Render();
-		ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-
-		return UPDATE_CONTINUE;
+		ImGui::EndMainMenuBar();
 	}
+
+	if (show_console)
+	{
+		ImGui::Begin("Configuration", NULL, ImGuiWindowFlags_MenuBar);
+		if (ImGui::CollapsingHeader("Application"))
+		{
+
+		}
+		if (ImGui::CollapsingHeader("Window"))
+		{
+
+		}
+		if (ImGui::CollapsingHeader("File System"))
+		{
+
+		}
+		if (ImGui::CollapsingHeader("Input"))
+		{
+
+		}
+		if (ImGui::CollapsingHeader("Hardware"))
+		{
+
+		}
+		//ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
+
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			show_another_window = false;
+		ImGui::End();
+	}
+
+	ImGui::Render();
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+	return UPDATE_CONTINUE;
 }
 				
 			
--- Monk Studio/Monk Studio/ModuleInterface.h
+++ Monk Studio/Monk Studio/ModuleInterface.h
@@ -17,6 +17,7 @@ public:
 
 	bool show_demo_window = false;
 	bool show_another_window = false;
+	bool show_console = true;
 
 
 };
