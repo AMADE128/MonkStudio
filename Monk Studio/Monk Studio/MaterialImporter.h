4ff784e317b02554207101146a02f7c78e8ad237
himarsus <60131783+himar33@users.noreply.github.com>
2021-11-19 09:51:38 +0100

    Uncomment code
    Just that

--- Monk Studio/Monk Studio/MaterialImporter.h
+++ Monk Studio/Monk Studio/MaterialImporter.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include "C_Material.h"
 #include <cstdint>
 #include "External Libraries/assimp/include/scene.h"
-//#include "C_Material.h"
+
+class MaterialFileFormat
+{
+public:
+	wchar_t name[20];
+	float version;
+
+	MaterialFileFormat()
+	{
+		wcsncpy(name, L"MaterialFileFormat", 20);
+		version = 1.0f;
+	}
+};
 
 namespace MaterialImporter
 {
-	//void Import(const aiMaterial* material, ComponentMaterial* ourMaterial);
-	//uint64_t Save(const ComponentMaterial* ourMaterial, char** fileBuffer);
-	//void Load(const char* fileBuffer, ComponentMaterial* ourMaterial);
+	void Import(const aiMaterial* material, ComponentMaterial* ourMaterial);
+	uint64_t Save(const ComponentMaterial* ourMaterial, char** fileBuffer);
+	void Load(const char* fileBuffer, ComponentMaterial* ourMaterial);
 
 }
